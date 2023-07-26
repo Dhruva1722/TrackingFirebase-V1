a5152c8842fe470c9d016fc4fb2b968989a3b838
Dhruva1722 <dhruvasolanki17@gmail.com>
2023-07-26 18:47:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/afinal/MapPresenter.kt
@@ -0,0 +1,55 @@
+package com.example.afinal
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MapPresenter(private val activity: AppCompatActivity) {
+
+    private val locationProvider = LocationProvider(activity)
+
+    private val stepCounter = StepCounter(activity)
+
+
+    private val permissionsManager = PermissionsManager(activity, locationProvider, stepCounter)
+
+    // Other methods and logic in the MapPresenter class
+
+    val ui = MutableLiveData(Ui.EMPTY)
+
+    fun onViewCreated() {
+//        TODO("Not yet implemented")
+        locationProvider.liveLocations.observe(activity) { locations ->
+            val current = ui.value
+            ui.postValue(current?.copy(userPath = locations))
+        }
+
+        locationProvider.liveLocation.observe(activity) { currentLocation ->
+            val current = ui.value
+            ui.postValue(current?.copy(currentLocation = currentLocation))
+        }
+
+        locationProvider.liveDistance.observe(activity) { distance ->
+            val current = ui.value
+            val formattedDistance = activity.getString(R.string.distance_value, distance)
+            ui.postValue(current?.copy(formattedDistance = formattedDistance))
+        }
+        permissionsManager.requestActivityRecognition()
+    }
+
+    fun onMapLoaded() {
+//        TODO("Not yet implemented")
+        permissionsManager.requestUserLocation()
+    }
+
+    fun startTracking() {
+//        TODO("Not yet implemented")
+        locationProvider.trackUser()
+    }
+
+    fun stopTracking() {
+        locationProvider.stopTracking()
+        stepCounter.unloadStepCounter()
+    }
+
+}
---
+++ app/src/main/java/com/example/afinal/MapsActivity.kt
@@ -0,0 +1,118 @@
+package com.example.afinal
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.afinal.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.PolylineOptions
+import com.example.afinal.MapPresenter
+import com.example.afinal.Ui
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private val presenter = MapPresenter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        binding.btnStartStop.setOnClickListener {
+            if (binding.btnStartStop.text == getString(R.string.start_label)) {
+                startTracking()
+                binding.btnStartStop.setText(R.string.stop_label)
+            } else {
+                stopTracking()
+                binding.btnStartStop.setText(R.string.start_label)
+            }
+        }
+
+        presenter.onViewCreated()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        presenter.ui.observe(this) { ui ->
+            updateUi(ui)
+        }
+
+        presenter.onMapLoaded()
+        map.uiSettings.isZoomControlsEnabled = true
+    }
+
+    private fun startTracking() {
+        binding.container.txtPace.text = ""
+        binding.container.txtDistance.text = ""
+        binding.container.txtTime.base = SystemClock.elapsedRealtime()
+        binding.container.txtTime.start()
+        map.clear()
+
+        presenter.startTracking()
+    }
+
+    private fun stopTracking() {
+        presenter.stopTracking()
+        binding.container.txtTime.stop()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateUi(ui: Ui) {
+        if (ui.currentLocation != null && ui.currentLocation != map.cameraPosition.target) {
+            map.isMyLocationEnabled = true
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(ui.currentLocation, 15f))
+
+            // Add marker at the current location
+            map.clear()
+
+            map.addMarker(
+                MarkerOptions()
+                    .position(ui.currentLocation)
+                    .title("Current Location")
+                    .snippet("Lat: ${ui.currentLocation.latitude}, Lng: ${ui.currentLocation.longitude}")
+            )
+        }
+
+        binding.container.txtDistance.text = ui.formattedDistance
+        binding.container.txtPace.text = ui.formattedPace
+        drawRoute(ui.userPath)
+    }
+
+    private fun drawRoute(locations: List<LatLng>) {
+        val polylineOptions = PolylineOptions()
+
+//        map.clear()
+
+        val points = polylineOptions.points
+        points.addAll(locations)
+
+        map.addPolyline(polylineOptions)
+    }
+}
